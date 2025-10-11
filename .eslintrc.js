eac9e803ea0488d9f93a24934a37142a1927f606
Kim Thái <thaihk@fanxipan.net>
2025-10-11 15:28:39 +0700

    refactor: add auto format and fix

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'simple-import-sort', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
